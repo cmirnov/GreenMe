885538c6f7f07796146074120a7bc33f81180a9c
cmirnov <cmirnov239kirill@gmail.com>
2018-04-14 00:47:20 +0300

    init

---
+++ app/src/main/java/com/example/kirill/greenme/MapFragmentView.java
@@ -0,0 +1,75 @@
+package com.example.kirill.greenme;
+
+import android.app.Activity;
+
+import java.util.Map;
+
+public class MapFragmentView {
+    private MapFragment m_mapFragment;
+    private Activity m_activity;
+    private Map m_map;
+
+    public MapFragmentView(Activity activity) {
+        m_activity = activity;
+        initMapFragment();
+    }
+
+    private void initMapFragment() {
+        /* Locate the mapFragment UI element */
+        m_mapFragment = (MapFragment) m_activity.getFragmentManager()
+                .findFragmentById(R.id.mapfragment);
+
+        // Set path of isolated disk cache
+        String diskCacheRoot = Environment.getExternalStorageDirectory().getPath()
+                + File.separator + ".isolated-here-maps";
+        // Retrieve intent name from manifest
+        String intentName = "";
+        try {
+            ApplicationInfo ai = m_activity.getPackageManager().getApplicationInfo(m_activity.getPackageName(),
+                    PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            intentName = bundle.getString("INTENT_NAME");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(this.getClass().toString(), "Failed to find intent name, NameNotFound: " + e.getMessage());
+        }
+
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(diskCacheRoot,
+                intentName);
+        if (!success) {
+            // Setting the isolated disk cache was not successful, please check if the path is valid and
+            // ensure that it does not match the default location
+            // (getExternalStorageDirectory()/.here-maps).
+            // Also, ensure the provided intent name does not match the default intent name.
+        } else {
+            if (m_mapFragment != null) {
+                /* Initialize the MapFragment, results will be given via the called back. */
+                m_mapFragment.init(new OnEngineInitListener() {
+                    @Override
+                    public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+
+                        if (error == Error.NONE) {
+                            /*
+                             * If no error returned from map fragment initialization, the map will be
+                             * rendered on screen at this moment.Further actions on map can be provided
+                             * by calling Map APIs.
+                             */
+                            m_map = m_mapFragment.getMap();
+
+                            /*
+                             * Map center can be set to a desired location at this point.
+                             * It also can be set to the current location ,which needs to be delivered by the PositioningManager.
+                             * Please refer to the user guide for how to get the real-time location.
+                             */
+
+                            m_map.setCenter(new GeoCoordinate(49.258576, -123.008268), Map.Animation.NONE);
+                        } else {
+                            Toast.makeText(m_activity,
+                                    "ERROR: Cannot initialize Map with error " + error,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
