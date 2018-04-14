a3b246bc107305dc0d1af9ce82ae38dbcfb7cc1f
sabrinamusatian <sabrina.musatian@yandex.ru>
2018-04-15 01:06:26 +0300

    working version of animation

--- app/src/main/java/com/example/kirill/greenme/FlowerGrowing.java
+++ app/src/main/java/com/example/kirill/greenme/FlowerGrowing.java
@@ -1,41 +1,115 @@
 package com.example.kirill.greenme;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class FlowerGrowing extends AppCompatActivity {
+public class FlowerGrowing extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private AnimationDrawable mAnimationDrawable;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_flower_growing);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_flower_growing);
+        setSupportActionBar(toolbar);
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setBackgroundResource(R.drawable.flowers_growing);
 
         mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
-
-        final Button btnStart = (Button) findViewById(R.id.buttonStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mAnimationDrawable.start();
-            }
-        });
-
-        final Button btnStop = (Button) findViewById(R.id.buttonStop);
-        btnStop.setOnClickListener(new View.OnClickListener() {
+//
+//        final Button btnStart = (Button) findViewById(R.id.buttonStart);
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                mAnimationDrawable.start();
+//            }
+//        });
+//
+        final FloatingActionButton btnDone = (FloatingActionButton) findViewById(R.id.TaskDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mAnimationDrawable.stop();
             }
         });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     @Override
     protected void onResume() {
